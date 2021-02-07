1426f86b8d5de6726ddd17c4160a4165e5f33267
Sam Elsharif <oelsha@engineer.com>
2021-02-06 19:16:53 -0500

    improved the look and feel of the scan popup dropdowns

--- src/app/popups/scan-parameters/scan-parameters.component.ts
+++ src/app/popups/scan-parameters/scan-parameters.component.ts
@@ -14,8 +14,12 @@ export class ScanParametersComponent implements OnInit {
 
   items: string[] = [];
   machines: string[] = [];
-  listing: string = null;
-  selectedMachine: string = ""
+  listings: string[] = [];
+  selectedOrEnteredListingDisplay: string = "";
+  selectedOrEnteredListing: string = "";
+  selectedListing: string = "";
+  enteredListing: string;
+  selectedMachine: string = "";
 
   constructor(private gamesService: GamesService) {}
 
@@ -28,14 +32,22 @@ export class ScanParametersComponent implements OnInit {
         self.close();
       }
     });
-
-    this.gamesService.getMachines().then((data: string[]) => { this.machines = data; this.selectedMachine = data[0] }); //Keep that for the bootstrap dropdown
   }
 
   open(): void {
     //intercept key board events to prevent them from propagating to the parent window
     document.addEventListener('keyup', this.handleKeyEvents);
     this.topNode.classList.add('scan-parameters-fade');
+
+    this.gamesService.getListings().then((data: string[]) => { this.listings = data; this.setSelectedListingAndAdjustForDisplay(data[0]); });
+    this.gamesService.getMachines().then((data: string[]) => {
+      this.machines = data;
+      if (this.selectedMachine == "") {
+        this.selectedMachine = data[0];
+      }
+    });
+
+    this.enteredListing = "";
   }
 
   close(): void {
@@ -48,13 +60,36 @@ export class ScanParametersComponent implements OnInit {
     this.items.splice(index, 1);
   }
 
+  setSelectedListing(listing: string) {
+    this.selectedOrEnteredListingDisplay = this.selectedOrEnteredListing = listing;
+    this.enteredListing = "";
+  }
+
+  setEnteredListing() {
+    if( this.enteredListing.trim() != "") {
+      this.setSelectedListingAndAdjustForDisplay(this.enteredListing, true);
+    } else {
+      this.enteredListing = "";
+    }
+    document.getElementById("listings-dropdown").classList.remove("show");
+  }
+
   submitParameters(form: any): void {
-    this.parameters.emit(new ScanParameters(this.items, this.listing, this.selectedMachine));
+    this.parameters.emit(new ScanParameters(this.items, this.selectedOrEnteredListing, this.selectedMachine));
     this.close();
   }
 
   private handleKeyEvents(event: any): void {
     event.stopPropagation();
+  }
+
+  private setSelectedListingAndAdjustForDisplay(listing: string, appendNew: boolean = false) {
+    this.selectedOrEnteredListing = listing;
+    if (appendNew) {
+      this.selectedOrEnteredListingDisplay = listing + ' (New)';
+    } else {
+      this.selectedOrEnteredListingDisplay = listing;
+    }
   }
 }
 
